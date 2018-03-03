dc0f8d62333ea64a1615e6bbd2d604ba6a9f5d41
Mikael <mikael.snellman@cs.helsinki.fi>
2018-03-03 15:46:21 +0200

    Mock successful response instead of really emailing for testing

--- mailer.js
+++ mailer.js
@@ -8,17 +8,25 @@ var transporter = nodemailer.createTransport({
 });
 
 exports.send = function (req, res) {
-  var mailOptions = {
-    from: 'BlueMix Reminder ✔',
-    to: req.body.to,
-    subject: req.body.subject,
-    html: req.body.body
-  };
-  transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-      console.log(error);
-        return res.status(500).json(error);
-    }
-    res.status(200).json(info.response);
-});
+//   var mailOptions = {
+//     from: 'BlueMix Reminder ✔',
+//     to: req.body.to,
+//     subject: req.body.subject,
+//     html: req.body.body
+//   };
+//   transporter.sendMail(mailOptions, function(error, info){
+//     if(error){
+//       console.log(error);
+//       return res.status(500).json(error);
+//     }
+//     res.status(200).json(info.response);
+//     console.log(new Date() + ": emailed: " + mailOptions.to)
+// });
+  const timeOut = Math.floor(Math.random() * (300 - 150)) + 150
+  setTimeout(returnOk(res), timeOut);
+  console.log(new Date() + ": sent ok after " + timeOut + "ms");
+}
+
+function returnOk(res) {
+  res.sendStatus(200);
 }
